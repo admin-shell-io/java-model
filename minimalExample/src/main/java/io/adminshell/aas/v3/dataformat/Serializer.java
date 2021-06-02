c6c448eb6bad37fcb5768719aa392749170c370c
Atlassian Bamboo <bamboo@example.com>
2021-06-02 14:14:27 +0200

    Updating raw Java classes to 1.0.12-SNAPSHOT

--- minimalExample/src/main/java/io/adminshell/aas/v3/dataformat/Serializer.java
+++ minimalExample/src/main/java/io/adminshell/aas/v3/dataformat/Serializer.java
@@ -16,17 +16,48 @@ import io.adminshell.aas.v3.model.*;
 import io.adminshell.aas.v3.model.impl.*;
 import io.adminshell.aas.v3.model.impl.builder.*;
 
+/***
+ * Generic serializer interface to serialize an instance of AssetAdministrationShellEnvironment to a
+ * string, Outputstream or java.io.File
+ */
 public interface Serializer {
 
+    /*** Default charset that will be used when no charset is specified */
     public static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
 
-    public String write(AssetAdministrationShellEnvironment aasEnvironment) throws IOException, SerializationException;
+    /***
+     * Serializes a given instance of AssetAdministrationShellEnvironment to string
+     * 
+     * @param aasEnvironment the AssetAdministrationShellEnvironment to serialize
+     * @return the string representation of the environment
+     * @throws SerializationException if serialization fails
+     */
+    public String write(AssetAdministrationShellEnvironment aasEnvironment) throws SerializationException;
 
+    /***
+     * Serializes a given instance of AssetAdministrationShellEnvironment to an OutputStream using
+     * DEFAULT_CHARSET
+     * 
+     * @param out the Outputstream to serialize to
+     * @param aasEnvironment the AssetAdministrationShellEnvironment to serialize
+     * @throws IOException if writing to the stream fails
+     * @throws SerializationException if serialization fails
+     */
     public default void write(OutputStream out, AssetAdministrationShellEnvironment aasEnvironment)
         throws IOException, SerializationException {
         write(out, DEFAULT_CHARSET, aasEnvironment);
     }
 
+    /**
+     * Serializes a given instance of AssetAdministrationShellEnvironment to an OutputStream using given
+     * charset
+     * 
+     * @param out the Outputstream to serialize to
+     * @param charset the Charset to use for serialization
+     * @param aasEnvironment the AssetAdministrationShellEnvironment to serialize
+     * @throws IOException if writing to the stream fails
+     * @throws SerializationException if serialization fails
+     */
     public default void write(OutputStream out, Charset charset, AssetAdministrationShellEnvironment aasEnvironment)
         throws IOException, SerializationException {
         try (OutputStreamWriter writer = new OutputStreamWriter(out, charset)) {
@@ -36,6 +67,17 @@ public interface Serializer {
 
     // Note that the AAS also defines a file class
 
+    /**
+     * Serializes a given instance of AssetAdministrationShellEnvironment to a java.io.File using
+     * DEFAULT_CHARSET
+     * 
+     * @param file the java.io.File to serialize to
+     * @param charset the Charset to use for serialization
+     * @param aasEnvironment the AssetAdministrationShellEnvironment to serialize
+     * @throws FileNotFoundException if the fail does not exist
+     * @throws IOException if writing to the file fails
+     * @throws SerializationException if serialization fails
+     */
     public default void write(java.io.File file, Charset charset, AssetAdministrationShellEnvironment aasEnvironment)
         throws FileNotFoundException, IOException, SerializationException {
         try (OutputStream out = new FileOutputStream(file)) {
@@ -43,6 +85,16 @@ public interface Serializer {
         }
     }
 
+    /**
+     * Serializes a given instance of AssetAdministrationShellEnvironment to a java.io.File using given
+     * charset
+     * 
+     * @param file the java.io.File to serialize to
+     * @param aasEnvironment the AssetAdministrationShellEnvironment to serialize
+     * @throws FileNotFoundException if the fail does not exist
+     * @throws IOException if writing to the file fails
+     * @throws SerializationException if serialization fails
+     */
     public default void write(java.io.File file, AssetAdministrationShellEnvironment aasEnvironment)
         throws FileNotFoundException, IOException, SerializationException {
         write(file, DEFAULT_CHARSET, aasEnvironment);
--- src/main/java/io/adminshell/aas/v3/dataformat/Deserializer.java
+++ src/main/java/io/adminshell/aas/v3/dataformat/Deserializer.java
@@ -17,16 +17,47 @@ import io.adminshell.aas.v3.model.*;
 import io.adminshell.aas.v3.model.impl.*;
 import io.adminshell.aas.v3.model.impl.builder.*;
 
+/**
+ * * Generic deserializer interface to deserialize a given string, Outputstream or java.io.File into
+ * an instance of AssetAdministrationShellEnvironment
+ */
 public interface Deserializer {
 
+    /*** Default charset that will be used when no charset is specified */
     public static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
 
+    /***
+     * Deserializes a given string into an instance of AssetAdministrationShellEnvironment
+     * 
+     * @param value a string representation of the AssetAdministrationShellEnvironment
+     * @return an instance of AssetAdministrationShellEnvironment
+     * @throws DeserializationException if deserialization fails
+     */
     public AssetAdministrationShellEnvironment read(String value) throws DeserializationException;
 
+    /***
+     * Deserializes a given InputStream into an instance of AssetAdministrationShellEnvironment using
+     * DEFAULT_CHARSET
+     * 
+     * @param src an InputStream containing the string representation of the
+     *        AssetAdministrationShellEnvironment
+     * @return an instance of AssetAdministrationShellEnvironment
+     * @throws DeserializationException if deserialization fails
+     */
     public default AssetAdministrationShellEnvironment read(InputStream src) throws DeserializationException {
         return read(src, DEFAULT_CHARSET);
     }
 
+    /***
+     * Deserializes a given InputStream into an instance of AssetAdministrationShellEnvironment using a
+     * given charset
+     * 
+     * @param src An InputStream containing the string representation of the
+     *        AssetAdministrationShellEnvironment
+     * @param charset the charset to use for deserialization
+     * @return an instance of AssetAdministrationShellEnvironment
+     * @throws DeserializationException if deserialization fails
+     */
     public default AssetAdministrationShellEnvironment read(InputStream src, Charset charset) throws DeserializationException {
         return read(new BufferedReader(
             new InputStreamReader(src, charset))
@@ -36,15 +67,49 @@ public interface Deserializer {
 
     // Note that the AAS also defines a file class
 
+    /***
+     * Deserializes a given File into an instance of AssetAdministrationShellEnvironment using
+     * DEFAULT_CHARSET
+     * 
+     * @param file A java.io.File containing the string representation of the
+     *        AssetAdministrationShellEnvironment
+     * @param charset the charset to use for deserialization
+     * @return an instance of AssetAdministrationShellEnvironment
+     * @throws FileNotFoundException if file is not present
+     * @throws DeserializationException if deserialization fails
+     */
     public default AssetAdministrationShellEnvironment read(java.io.File file, Charset charset)
         throws FileNotFoundException, DeserializationException {
         return read(new FileInputStream(file), charset);
     }
 
+    /***
+     * Deserializes a given File into an instance of AssetAdministrationShellEnvironment using a given
+     * charset
+     * 
+     * @param file a java.io.File containing the string representation of the
+     *        AssetAdministrationShellEnvironment
+     * @return an instance of AssetAdministrationShellEnvironment
+     * @throws FileNotFoundException if the file is not present
+     * @throws DeserializationException if deserialization fails
+     */
     public default AssetAdministrationShellEnvironment read(java.io.File file) throws FileNotFoundException, DeserializationException {
         return read(file, DEFAULT_CHARSET);
     }
 
+    /***
+     * Enables usage of custom implementation to be used for deserialization instead of default
+     * implementation, e.g. defining a custom implementation of the Submodel interface {@code class
+     * CustomSubmodel implements Submodel {}} and calling
+     * {@code useImplementation(Submodel.class, CustomSubmodel.class);} will result in all instances of
+     * Submodel will be deserialized as CustomSubmodel. Subsequent class with the same aasInterface
+     * parameter will override the effects of all previous calls.
+     * 
+     * @param <T> the type of the interface to replace
+     * @param aasInterface the class of the interface to replace
+     * @param implementation the class implementing the interface that should be used for
+     *        deserialization.
+     */
     public <T> void useImplementation(Class<T> aasInterface, Class<? extends T> implementation);
 
 }
