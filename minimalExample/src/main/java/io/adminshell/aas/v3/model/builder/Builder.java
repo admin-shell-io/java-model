26431d380b6753fe10ec0cf1a111e7919020f6a9
Sebastian Bader <sebastian.r.bader@web.de>
2021-06-25 17:01:00 +0200

    Removing the Minimal Example

--- minimalExample/src/main/java/io/adminshell/aas/v3/model/builder/Builder.java
+++
@@ -1,7 +0,0 @@
-package io.adminshell.aas.v3.model.builder;
-
-public interface Builder<T> {
-
-    public T build();
-
-}
--- minimalExample/src/main/java/io/adminshell/aas/v3/model/builder/ExtendableBuilder.java
+++
@@ -1,7 +0,0 @@
-package io.adminshell.aas.v3.model.builder;
-
-public abstract class ExtendableBuilder<T, B extends ExtendableBuilder<T, B>> extends AbstractBuilder<T> {
-
-    protected abstract B getSelf();
-
-}
