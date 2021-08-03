6f25280c772c2f8e33946443b0ae86004c8971cd
Atlassian Bamboo <bamboo@example.com>
2021-08-03 16:07:09 +0200

    Updating raw Java classes to 1.0.3-SNAPSHOT

--- src/main/java/io/adminshell/aas/v3/model/LevelType.java
+++ src/main/java/io/adminshell/aas/v3/model/LevelType.java
@@ -1,18 +1,17 @@
 /*
  * Copyright (c) 2021 Fraunhofer-Gesellschaft zur Foerderung der angewandten Forschung e. V.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
+
 package io.adminshell.aas.v3.model;
 
 
@@ -32,21 +31,25 @@ public enum LevelType {
     /**
      * Max according to IEC 61360 as an Identifier scheme for maximal levels.
      */
+    @IRI("https://admin-shell.io/aas/3/0/RC01/LevelType/Max")
     MAX,
 
     /**
      * Min according to IEC 61360 as an Identifier scheme for minimal levels.
      */
+    @IRI("https://admin-shell.io/aas/3/0/RC01/LevelType/Min")
     MIN,
 
     /**
      * Nom according to IEC 61360 as an Identifier scheme for nominal levels.
      */
+    @IRI("https://admin-shell.io/aas/3/0/RC01/LevelType/Nom")
     NOM,
 
     /**
      * Typ according to IEC 61360 as an Identifier scheme for typical levels.
      */
+    @IRI("https://admin-shell.io/aas/3/0/RC01/LevelType/Typ")
     TYP;
 
 }
