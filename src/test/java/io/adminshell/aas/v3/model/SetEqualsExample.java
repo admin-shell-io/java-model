5541990fe0b3f01547df4d631f25d390af7e407c
br-iosb <70897399+br-iosb@users.noreply.github.com>
2021-09-09 08:50:39 +0200

    Create SetEqualsExample.java
    simple equals test

---
+++ src/test/java/io/adminshell/aas/v3/model/SetEqualsExample.java
@@ -0,0 +1,34 @@
+package io.adminshell.aas.v3.model;
+
+import com.google.common.base.Objects;
+
+import io.adminshell.aas.v3.model.impl.DefaultAssetAdministrationShellEnvironment;
+import io.adminshell.aas.v3.model.impl.DefaultIdentifier;
+import io.adminshell.aas.v3.model.impl.DefaultSubmodel;
+
+public class SetEqualsExample {
+	
+	public static void main(String[] args) {
+		DefaultSubmodel sm_1 = new DefaultSubmodel();
+		sm_1.setIdentification(new DefaultIdentifier.Builder().identifier("sm_1").build());
+		DefaultSubmodel sm_2 = new DefaultSubmodel();
+		sm_2.setIdentification(new DefaultIdentifier.Builder().identifier("sm_2").build());
+		
+		DefaultAssetAdministrationShellEnvironment env1 = new DefaultAssetAdministrationShellEnvironment();
+		DefaultAssetAdministrationShellEnvironment env2 = new DefaultAssetAdministrationShellEnvironment();
+		env1.getSubmodels().add(sm_1);
+		env1.getSubmodels().add(sm_2);
+		env2.getSubmodels().add(sm_1);
+		env2.getSubmodels().add(sm_2);
+		System.out.println("Environment are equal:" +  Objects.equal(env1, env2));
+		
+		DefaultAssetAdministrationShellEnvironment env3 = new DefaultAssetAdministrationShellEnvironment();
+		DefaultAssetAdministrationShellEnvironment env4 = new DefaultAssetAdministrationShellEnvironment();
+		env3.getSubmodels().add(sm_1);
+		env3.getSubmodels().add(sm_2);
+		env4.getSubmodels().add(sm_2);
+		env4.getSubmodels().add(sm_1);
+		System.out.println("Environment are equal:" +  Objects.equal(env3, env4));
+	}
+
+}
